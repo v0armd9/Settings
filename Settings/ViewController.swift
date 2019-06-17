c530969d9265aab6faa897c095561a5bf28ad36b
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-17 10:16:34 -0600

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Darin Marcus Armstrong on 6/17/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
