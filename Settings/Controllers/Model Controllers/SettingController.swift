80b04ddc3740a0fb8b32fdf79d4a8f34045726fd
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-17 11:23:44 -0600

    Completed SettingController File - Created the setting array and function allowing for the toggle of the isOn Boolean

--- Settings/Controllers/Model Controllers/SettingController.swift
+++ Settings/Controllers/Model Controllers/SettingController.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class SettingController {
+    
+    static let sharedInstance = SettingController()
+    
+    let settings: [Setting] = {
+        let music = Setting(name: "iTunes", icon: #imageLiteral(resourceName: "itunes"), isOn: false)
+        let appStore = Setting(name: "App Store", icon: #imageLiteral(resourceName: "appstore"), isOn: false)
+        let iBooks = Setting(name: "iBooks", icon: #imageLiteral(resourceName: "ibooks"), isOn: true)
+        
+        return[music, appStore, iBooks]
+    }()
+    
+    func toggleIsOn(for setting: Setting) {
+        setting.isOn = !setting.isOn
+    }
+}
