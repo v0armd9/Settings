96e876559f751fe635a1ed0774ea256e46d5490d
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-17 11:00:08 -0600

    Completed Model - Completed the model.swift file and linked cell in storyboard to custom cell view

--- Settings/Models/Setting.swift
+++ Settings/Models/Setting.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Setting {
+    let name: String
+    let icon: UIImage
+    var isOn: Bool
+    
+    init(name: String, icon: UIImage, isOn: Bool) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
